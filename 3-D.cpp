7d514ea1d78fc81d072100e789a018859267d862
Иван Белецкий <63805710+ibeletskiy@users.noreply.github.com>
2024-12-20 23:55:12 +0300

    Update 3-D.cpp

--- 3-D.cpp
+++ 3-D.cpp
@@ -25,66 +25,64 @@
 #define v std::vector
 
 using Ll = long long;
+using Ld = long double;
 using Str = std::string;
 using Pll = std::pair<Ll, Ll>;
 
-// const Ll cMax = 1e17 + 1, cMin = -1e17 - 1;
+const Ll cMax = 1e17 + 1;
+// const Ll cMin = -1e17 - 1;
 // const Ll cPow = 42;
-// const Ll cMod = 1234567891;
+// const Ll c_mod = 1e9 + 7;
 // const Ll cLg = 20;
 // const int cMaxn = 1e6 + 10;
 
-void MakeSuf(const Str& s, v<Ll>& a, v<Ll>& c) {
-  v<std::pair<char, Ll>> tmp;
-  Ll n = s.size();
-  a.resize(n);
-  c.resize(n);
-  for (int i = 0; i < n; ++i) {
-    tmp.push_back({s[i], i});
+Ll Sum(Ll a, Ll b, Ll c_mod) { return (a + b) % c_mod; }
+
+Ll Sub(Ll a, Ll b, Ll c_mod) {
+  a -= b;
+  while (a < 0) {
+    a += c_mod;
   }
-  std::sort(all(tmp));
-  v<Ll> first(n);
-  Ll cc = -1;
-  for (int i = 0; i < n; ++i) {
-    a[i] = tmp[i].second;
-    if (i == 0 || tmp[i].first != tmp[i - 1].first) {
-      ++cc;
-      first[cc] = i;
+  return a;
+}
+
+Ll Mul(Ll a, Ll b, Ll c_mod) { return (a * b) % c_mod; }
+
+Ll Binpow(Ll a, Ll n, Ll c_mod) {
+  Ll ans = 1;
+  while (n != 0) {
+    if ((n & 1) != 0) {
+      ans = Mul(ans, a, c_mod);
     }
-    c[a[i]] = cc;
+    a = Mul(a, a, c_mod);
+    n = n >> 1;
   }
-  v<Ll> a1(n);
-  v<Ll> c1(n);
-  Ll len = 1;
-  while (len < n) {
-    for (int i = 0; i < n; ++i) {
-      Ll j = (a[i] - len + n) % n;
-      a1[first[c[j]]++] = j;
-    }
-    a = a1;
-    Ll cc = -1;
-    for (int i = 0; i < n; ++i) {
-      if (i == 0 || c[a[i]] != c[a[i - 1]] ||
-          c[(a[i] + len + n) % n] != c[(a[i - 1] + len + n) % n]) {
-        ++cc;
-        first[cc] = i;
-      }
-      c1[a[i]] = cc;
-    }
-    c = c1;
-    len *= 2;
-  }
+  return ans;
 }
 
 void Solve() {
-  Str s;
-  std::cin >> s;
-  v<Ll> a;
-  v<Ll> c;
-  Ll n = s.size();
-  MakeSuf(s, a, c);
-  for (int i = 0; i < n; ++i) {
-    std::cout << s[(a[i] + n - 1) % n];
+  Ll p;
+  Ll b;
+  Ll n;
+  while (std::cin >> p >> b >> n) {
+    Ll c_root = static_cast<Ll>(sqrt(static_cast<Ld>(p)) + 1);
+    Ll ans = cMax;
+    std::unordered_map<Ll, Ll> value;
+    for (Ll i = c_root; i >= 1; --i) {
+      value[Binpow(b, i * c_root, p)] = i;
+    }
+    for (Ll i = 0; i <= c_root; ++i) {
+      Ll now = Mul(Binpow(b, i, p), n, p);
+      if (value.find(now) != value.end()) {
+        Ll tmp = value[now] * c_root - i;
+        ans = std::min(tmp, ans);
+      }
+    }
+    if (ans == cMax) {
+      std::cout << "no solution\n";
+    } else {
+      std::cout << ans << '\n';
+    }
   }
 }
 
